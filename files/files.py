fcc099c25f9c272d094954cf66a40080822c615e
selvakumar kamatchinathan <selva.439@gmail.com>
2024-09-25 18:48:52 +0100

    refactor

--- files/files.py
+++ files/files.py
@@ -87,14 +87,94 @@ class Files:
         """
         for file in file_list_json:
             if file['publicFileLocations'][0]['name'] == 'FTP Protocol':
-                ftp_filepath = file['publicFileLocations'][0]['value']
+                download_url = file['publicFileLocations'][0]['value']
             else:
-                ftp_filepath = file['publicFileLocations'][1]['value']
-            logging.debug('ftp_filepath:' + ftp_filepath)
-            public_filepath_part = ftp_filepath.rsplit('/', 1)
-            logging.debug(file['accession'] + " -> " + public_filepath_part[1])
-            new_file_path = file['accession'] + "-" + public_filepath_part[1]
-            urllib.request.urlretrieve(ftp_filepath, output_folder + new_file_path)
+                download_url = file['publicFileLocations'][1]['value']
+            logging.debug('ftp_filepath:' + download_url)
+            new_file_path = Files.get_output_file_name(download_url, file, output_folder)
+            urllib.request.urlretrieve(download_url, new_file_path)
+
+    @staticmethod
+    def get_output_file_name(download_url, file, output_folder):
+        public_filepath_part = download_url.rsplit('/', 1)
+        logging.debug(file['accession'] + " -> " + public_filepath_part[1])
+        new_file_path = os.path.join(output_folder, file['accession'] + "-" + public_filepath_part[1])
+        return new_file_path
+
+    @staticmethod
+    def download_files_from_aspera(file_list_json, output_folder):
+        """
+        Download files using ftp transfer url
+        :param file_list_json: file list in json format
+        :param output_folder: folder to download the files
+        """
+        ascp_path = Files.get_ascp_binary()
+        key_path = os.path.abspath('aspera/key/asperaweb_id_dsa.openssh')
+        for file in file_list_json:
+            if file['publicFileLocations'][0]['name'] == 'Aspera Protocol':
+                download_url = file['publicFileLocations'][0]['value']
+            else:
+                download_url = file['publicFileLocations'][1]['value']
+
+            # Create a clean filename to save the downloaded file
+            logging.debug(f'Downloading via Aspera: {download_url}')
+            new_file_path = Files.get_output_file_name(download_url, file, output_folder)
+            try:
+                # Execute the ascp command using subprocess
+                subprocess.run([
+                    ascp_path, '-QT', '-P', '33001', '-l', '100M',  # Options for Aspera: adjust as necessary
+                    '-i', key_path,
+                    download_url, new_file_path  # Source and destination
+                ], check=True)
+                logging.info(f'Successfully downloaded {new_file_path} via Aspera')
+            except subprocess.CalledProcessError as e:
+                logging.error(f'Aspera download failed for {new_file_path}: {str(e)}')
+
+    @staticmethod
+    def download_files_from_globus(file_list_json, output_folder):
+        for file in file_list_json:
+            if file['publicFileLocations'][0]['name'] == 'Aspera Protocol':
+                download_url = file['publicFileLocations'][0]['value']
+            else:
+                download_url = file['publicFileLocations'][1]['value']
+            logging.debug(f'Downloading fron Globus: {download_url}')
+            ftp_base_url = "ftp://ftp.pride.ebi.ac.uk/"
+            globus_base_url = "https://g-a8b222.dd271.03c0.data.globus.org/"
+            download_url = download_url.replace(ftp_base_url, globus_base_url)
+            # Globus download using urllib
+            logging.debug(f'Downloading From Globus: {download_url}')
+            # Create a clean filename to save the downloaded file
+            new_file_path = Files.get_output_file_name(download_url, file, output_folder)
+            try:
+                urllib.request.urlretrieve(download_url, new_file_path)
+                logging.info(f'Successfully downloaded {new_file_path}')
+            except Exception as e:
+                logging.error(f'Download from globus failed for {new_file_path}: {str(e)}')
+
+    def download_file_from_s3(self, file_name, path, output_folder):
+        """
+        Download files from s3 bucket
+        :param file_name: file name
+        :param path: path in s3
+        :param output_folder: folder to download the files
+        """
+        if not (os.path.isdir(output_folder)):
+            os.mkdir(output_folder)
+
+        s3_resource = boto3.resource('s3', config=Config(signature_version=botocore.UNSIGNED), endpoint_url=self.S3_URL)
+
+        try:
+            bucket = s3_resource.Bucket(self.S3_BUCKET)
+            bucket.download_file(path + '/' + file_name, os.path.join(output_folder, file_name))
+            objects_all = bucket.objects.filter(Prefix=path)
+            print(objects_all)
+            for item in objects_all:
+                print(item)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
 
     def get_submitted_file_path_prefix(self, accession):
         """
@@ -137,7 +217,7 @@ class Files:
         """
 
         # get the full path where you can find the raw files in the file system
-        # to support that, data should be written in following format:
+        # to support that, data should be written in the following format:
         # base/path/ + yyyy/mm/accession/ + submitted/
         path_fragment = self.get_submitted_file_path_prefix(accession)
         complete_source_dir = source_base_directory + "/" + path_fragment + "/submitted/"
@@ -252,60 +332,9 @@ class Files:
             logging.error('Protocol should be either ftp, aspera, globus')
             return
 
-        if protocol == 'aspera':
-            ascp_path = Files.get_ascp_binary()
-            key_path = os.path.abspath('aspera/key/asperaweb_id_dsa.openssh')
-
-        for file in file_list_json:
-            protocol_name = file['publicFileLocations'][0]['name']
-            download_url = file['publicFileLocations'][0]['value']
-
-            # Create a clean filename to save the downloaded file
-            public_filepath_part = download_url.rsplit('/', 1)
-            logging.debug(file['accession'] + " -> " + public_filepath_part[1])
-            new_file_path = os.path.join(output_folder, file['accession'] + "-" + public_filepath_part[1])
-
-            # Check if the protocol is FTP or Aspera
-            if protocol == 'ftp':
-                if protocol_name == 'Aspera Protocol':
-                    download_url = file['publicFileLocations'][1]['value']
-
-                # FTP download using urllib
-                logging.debug(f'Downloading via FTP: {download_url}')
-                try:
-                    urllib.request.urlretrieve(download_url, new_file_path)
-                    logging.info(f'Successfully downloaded {new_file_path}')
-                except Exception as e:
-                    logging.error(
-                        f'FTP download failed for {new_file_path} , please trying using aspera or globus: {str(e)}')
-                    raise e
-
-            elif protocol == 'aspera':
-                if protocol_name == 'FTP Protocol':
-                    download_url = file['publicFileLocations'][1]['value']
-                # Aspera download using ascp command
-                ascp_source_url = download_url  # The Aspera URL from the JSON
-                logging.debug(f'Downloading via Aspera: {ascp_source_url}')
-                try:
-                    # Execute the ascp command using subprocess
-                    subprocess.run([
-                        ascp_path, '-QT', '-P', '33001', '-l', '100M',  # Options for Aspera: adjust as necessary
-                        '-i', key_path,
-                        ascp_source_url, new_file_path  # Source and destination
-                    ], check=True)
-                    logging.info(f'Successfully downloaded {new_file_path} via Aspera')
-                except subprocess.CalledProcessError as e:
-                    logging.error(f'Aspera download failed for {new_file_path}: {str(e)}')
-            elif protocol == 'globus':
-                if protocol_name == 'Aspera Protocol':
-                    download_url = file['publicFileLocations'][1]['value']
-                ftp_base_url = "ftp://ftp.pride.ebi.ac.uk/"
-                globus_base_url = "https://g-a8b222.dd271.03c0.data.globus.org/"
-                download_url = download_url.replace(ftp_base_url, globus_base_url)
-                # Globus download using urllib
-                logging.debug(f'Downloading From Globus: {download_url}')
-                try:
-                    urllib.request.urlretrieve(download_url, new_file_path)
-                    logging.info(f'Successfully downloaded {new_file_path}')
-                except Exception as e:
-                    logging.error(f'Download from globus failed for {new_file_path}: {str(e)}')
+        if protocol == 'ftp':
+            Files.download_files_from_ftp(file_list_json, output_folder)
+        elif protocol == 'aspera':
+            Files.download_files_from_aspera(file_list_json, output_folder)
+        elif protocol == 'globus':
+            Files.download_files_from_globus(file_list_json, output_folder)
